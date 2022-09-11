74620f7e51b1d05001c7c6f432e1bc3797bdcdbc
Nathalya Amorim <nathalyamorim87@gmail.com>
2022-09-10 23:05:13 -0400

    Adding footer and contact. Adding Portfolio page.

--- js/main.js
+++ js/main.js
@@ -4,23 +4,48 @@
     let teammate2 = document.querySelector("#teammate2");
     let teammate3 = document.querySelector("#teammate3");
     let teammate4 = document.querySelector("#teammate4");
-    let overlayPanel = document.querySelector("#profile-overlay-panel");
+
+    let profileOverlayPanel = document.querySelector("#profile-overlay-panel");
     let btnCloseProfileOverlay = document.querySelector("#close-profile-overlay");
+
+    let product1 = document.querySelector("#product1");
+    let product2 = document.querySelector("#product2");
+    let product3 = document.querySelector("#product3");
+    let product4 = document.querySelector("#product4");
+    let product5 = document.querySelector("#product5");
+    let product6 = document.querySelector("#product6");
+
+    let portfolioOverlayPanel = document.querySelector("#portfolio-overlay-panel");
+    let btnClosePortfolioOverlay = document.querySelector("#close-portfolio-overlay");
 
     function openProfile() {
         var profileId = "#" + this.dataset.profile + "-profile";
 
-        overlayPanel.style.display = "block";
+        profileOverlayPanel.style.display = "block";
         document.querySelector(profileId).style.display = "block";
         btnCloseProfileOverlay.dataset.lastProfileId = profileId; 
     }
 
     function closeProfile() {
-        //TODO: Implement this
         var profileId = this.dataset.lastProfileId;
 
         document.querySelector(profileId).style.display = "none";
-        overlayPanel.style.display = "none";
+        profileOverlayPanel.style.display = "none";
+    }
+
+    function openPortfolioProduct() {
+        var productId = "#" + this.dataset.product + "-product";
+
+        portfolioOverlayPanel.style.display = "block";
+        document.querySelector(productId).style.display = "block";
+        btnClosePortfolioOverlay.dataset.lastProductId = productId; 
+    }
+
+    function closePortfolioProduct() {
+        var productId = this.dataset.lastProductId;
+
+        document.querySelector(productId).style.display = "none";
+        portfolioOverlayPanel.style.display = "none";
     }
 
     teammate1.addEventListener("click", openProfile);
@@ -28,5 +53,13 @@
     teammate3.addEventListener("click", openProfile);
     teammate4.addEventListener("click", openProfile);
     btnCloseProfileOverlay.addEventListener("click", closeProfile);
+
+    product1.addEventListener("click", openPortfolioProduct);
+    product2.addEventListener("click", openPortfolioProduct);
+    product3.addEventListener("click", openPortfolioProduct);
+    product4.addEventListener("click", openPortfolioProduct);
+    product5.addEventListener("click", openPortfolioProduct);
+    product6.addEventListener("click", openPortfolioProduct);
+    btnClosePortfolioOverlay.addEventListener("click", closePortfolioProduct);
 
 })();
